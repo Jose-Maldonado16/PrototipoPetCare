58458a1c94855311c222c5cd633be9a60800208c
Jose-Maldonado16 <maldonadoolmosjoseluis@gmail.com>
2026-05-22 20:33:58 -0400

    Sprint2

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://prototipopetcare-2.onrender.com/api';
 
 export const api = {
     // ==================== USUARIOS (Sprint 1) ====================
